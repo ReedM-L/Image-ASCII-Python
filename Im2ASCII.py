0193d7bad7b3c7e618447740c43ebae2ba052699
Reed Lyle <reedlyle@Reeds-Laptop.local>
2024-11-20 12:38:14 -0500

    Initial commit: Add python code for image-ASCII generator.

---
+++ Im2ASCII.py
@@ -0,0 +1,15 @@
+#Image to ASCII generator
+from PIL import Image
+
+##Open image and assign to variable
+im = Image.open('HSS.png', 'r')
+
+##Convert to grayscale
+gray_im = im.convert("L")
+##Map brightness of pixels
+
+
+##Format
+
+##Print out
+
